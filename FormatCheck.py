2ec65fafd51ff9078cf3ef926d740861e9e0f5af
Aaron Stanek <amr.stanek@gmail.com>
2021-07-13 11:46:58 -0700

    added check for setOfStrings

--- FormatCheck.py
+++ FormatCheck.py
@@ -5,8 +5,16 @@ def listOfStrings(x):
     # of nonempty strings
     # returns throws an exception otherwise
     if type(x) != list:
-        raise Exception("Expected list")
+        raise TypeError("Expected list")
     if len(x) == 0:
         raise ValueError("Expected nonempty list")
     if not all(map(lambda y: (len(y) > 0) if (type(y) == str) else False , x)):
         raise TypeError("Expected elements of list to be nonempty strings")
+
+def setOfStrings(x):
+    # returns is x is a set of strings
+    # throws and expection otherwise
+    if type(x) != set:
+        raise TypeError("Expected set")
+    if not all(map(lambda y: (len(y) > 0) if (type(y) == str) else False , x)):
+        raise TypeError("Expected elements of set to be nonempty strings")
