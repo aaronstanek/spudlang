4a285f48cf68d01ca185d5084954b2eb5e7c0226
Aaron Stanek <amr.stanek@gmail.com>
2021-07-13 11:57:34 -0700

    added dictStrBool

--- FormatCheck.py
+++ FormatCheck.py
@@ -12,9 +12,21 @@ def listOfStrings(x):
         raise TypeError("Expected elements of list to be nonempty strings")
 
 def setOfStrings(x):
-    # returns is x is a set of strings
+    # returns if x is a set of strings
     # throws and expection otherwise
     if type(x) != set:
         raise TypeError("Expected set")
     if not all(map(lambda y: (len(y) > 0) if (type(y) == str) else False , x)):
         raise TypeError("Expected elements of set to be nonempty strings")
+
+def dictStrBool(x):
+    # returns if x is dict(str->bool)
+    # throws exception otherwise
+    if type(x) != dict:
+        raise TypeError("Expected dict")
+    for key in x:
+        if type(key) != str:
+            raise ValueError("Expected dict with str keys")
+        value = x[key]
+        if type(value) != bool:
+            raise TypeError("Expected dict with str keys and bool values")
