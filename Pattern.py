18f4c141b7e37144b8d8ec2098d96629abf2ea75
Aaron Stanek <amr.stanek@gmail.com>
2021-07-13 11:34:14 -0700

    wrote SinglePAttern

--- Pattern.py
+++ Pattern.py
@@ -1,6 +1,14 @@
 # defines patterns which can be checked against for rule application
 # SinglePattern checks for a match in either the unit or the name
+# returns tuple (0,x) (1,x) (2,x)
+# 0 means no match
+# 1 means match in unit
+# 2 means match in name
+# x is index of the match in the sample
 # DoublePattern checks for a match in both the unit and name
+
+from FormatCheck import listOfStrings
+from Ingredient import Ingredient
 
 class Pattern(object):
     @staticmethod
@@ -19,3 +27,19 @@ class Pattern(object):
             if matches:
                 return sample_index
         return None
+
+class SinglePattern(Pattern):
+    def __init__(self,rule):
+        listOfStrings(rule)
+        self.rule = rule
+    def matches(self,ig):
+        if type(ig) != Ingredient:
+            raise TypeError("Expected ig to be of type Ingredient")
+        res = self._compare(self.rule,ig.name)
+        if res is not None:
+            return (2,res)
+        res = self._compare(self.rule,ig.unit)
+        if res is not None:
+            return (1,res)
+        else:
+            return (0,None)
