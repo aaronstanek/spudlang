9efacae50e88a24382d9b261d9edde7292d06c7d
Aaron Stanek <amr.stanek@gmail.com>
2021-07-19 14:50:28 -0700

    wrote PrefixingRuleOutput.apply

--- RuleOutput.py
+++ RuleOutput.py
@@ -9,6 +9,7 @@ class RuleOutput(object):
     def __init__(self):
         raise Exception("creating an instance of RuleOutput base class is forbidden")
     def check_conformity(ig,match_token,match_token_length):
+        # check that an input to apply makes sense
         if type(ig) != Ingredient:
             raise TypeError("Expected ig to be an Ingredient")
         if type(match_token) != tuple:
@@ -26,14 +27,7 @@ class RenamingRuleOutput(RuleOutput):
         self.output_name = output_name
     def apply(self,ig,match_token):
         # match token must come from SinglePattern
-        if type(ig) != Ingredient:
-            raise TypeError("Expected ig to be an Ingredient")
-        if type(match_token) != tuple:
-            raise TypeError("Expected tuple")
-        if len(match_token) != 2:
-            raise ValueError("Expected tuple of length 2")
-        if not all(map(lambda x: type(x) == int, match_token)):
-            raise TypeError("Expected tuple filled with ints")
+        self.check_conformity(ig,match_token,2)
         output = ig.duplicate()
         if match_token[0] == 1:
             # editing unit
@@ -47,3 +41,19 @@ class PrefixingRuleOutput(RuleOutput):
     def __init__(self,prefix):
         listOfStrings(prefix)
         self.prefix = prefix
+    def apply(self,ig,match_token):
+        # match token must come from SinglePattern
+        self.check_conformity(ig,match_token,2)
+        if match_token[1] != 0:
+            # if the input already has a prefix,
+            # we shouldn't change it
+            return ig
+        output = ig.duplicate()
+        if match_token[0] == 1:
+            # editing unit
+            # we know that match_token[1] == 0
+            output.unit = self.prefix + output.unit
+        else:
+            # editing name
+            output.name = self.prefix + output.name
+        return output
