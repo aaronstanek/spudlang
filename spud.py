ffa4819119b408ade4d398ec68a75a59661b3adb
Aaron Stanek <amr.stanek@gmail.com>
2021-07-27 16:33:20 -0700

    created import system

---
+++ spud.py
@@ -0,0 +1,60 @@
+# the main file of the program
+
+import sys
+import os
+import hashlib
+import Parse
+
+def load_file_raw(path):
+    if path[-5:] != ".spud":
+        path = path + ".spud"
+    if os.path.isfile(path):
+        with open(path,"rb") as file:
+            return file.read()
+    else:
+        raise Exception("Unable to locate file: "+path)
+
+def convert_to_absolute_pathnames(base,path):
+    if os.path.isabs(path):
+        return path
+    
+
+def recursive_load(path,seen=set()):
+    # recursively loads all input files
+    raw_data = load_file_raw(path)
+    hex_hash = hashlib.sha256(raw_data).hexdigest()
+    if hex_hash in seen:
+        # we have already loaded this file
+        return []
+    else:
+        # hex_hash has not been seen yet
+        seen.add(hex_hash)
+    raw_data = Parse.convert_equivalent_codes(raw_data)
+    raw_data = Parse.make_lines(raw_data)
+    raw_data = list(filter(lambda x: not Parse.is_empty(x), raw_data))
+    raw_data = list(filter(lambda x: not Parse.is_comment(x), raw_data))
+    # raw_data is now a list of strings
+    # there are no empty lines
+    # and no comments
+    imports = list(filter(lambda x: Parse.is_import(x), raw_data))
+    imports = list(map(lambda x: Parse.interpret_import(x), imports))
+    output = []
+    for import_path in imports:
+        if not os.path.isabs(import_path):
+            import_path = os.join(os.path.dirname(),import_path)
+            import_path = os.path.abspath(import_path)
+        output += recursive_load(import_path,seen)
+    raw_data = list(filter(lambda x: not Parse.is_import(x), raw_data))
+    output += raw_data
+    return output
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception("Expected a file to process")
+    if os.path.isabs(sys.argv[1]):
+        initial_path = sys.argv[1]
+    else:
+        initial_path = os.path.join(os.getcwd(),sys.argv[1])
+
+if __name__ == "__main__":
+    main()
