bf9676e4a4c429f57e735f8eaf14dd8beb53ef47
Aaron Stanek <amr.stanek@gmail.com>
2021-08-04 11:22:40 -0700

    parse_pattern

--- Parse2.py
+++ Parse2.py
@@ -1,6 +1,7 @@
 # second attempt at writing a parser
 
 from Lex2 import StandardLineLexer, AtCommandLexer
+import Pattern
 import RuleOutput
 
 def resolve_begin_end(lines):
@@ -25,6 +26,28 @@ def resolve_begin_end(lines):
             raise Exception("Internal Error")
     if len(factors) != 0:
         raise Exception("Parse Error: @begin without corresponding @end")
+
+def create_props_dict(props_tuple_array):
+    output = {}
+    for i in range(len(props_tuple_array)-1,-1,-1):
+        key = props_tuple_array[i][0]
+        value = props_tuple_array[i][1]
+        output[key] = value
+    return output
+
+def parse_pattern(noun):
+    # noun is a NounLexer object
+    # we will return the pattern associated with it
+    if noun.props is None:
+        props_dict = {}
+    else:
+        props_dict = create_props_dict(noun.props.props)
+    if noun.unit is None:
+        # Single Pattern
+        return Pattern.SinglePattern(noun.name.noun_core,props_dict)
+    else:
+        # Double Pattern
+        return Pattern.DoublePattern(noun.unit.noun_core,noun.name.noun_core,props_dict)
 
 def parse_properties_edit(base,noun):
     # rule is a RuleOutput of some kind
